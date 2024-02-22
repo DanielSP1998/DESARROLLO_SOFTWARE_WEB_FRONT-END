27afb69c6463cacba8e10af98ba1050ebc521d9c
DanielSP1998 <danielsp_1998@hotmail.com>
2024-02-22 18:32:12 -0500

    inicio de aplicación desarrollo fron-end

---
+++ Primer_Corte/Proyecto_Desarrollo_Front/JS/Encabezado_PiePagina.js
@@ -0,0 +1,70 @@
+const Header = `<section class="header">
+<div class="Contactenos">
+    <a href="contactenos.html" target="_blank">
+        <img src="Img/header_2.jpg" alt="Vencer o Morir">
+        <br>
+        Contactenos
+    </a>
+</div>
+<div class="Inicio">
+    <a href="Index.html">
+        <img src="Img/Header.png" alt="Nosotros Somo Bogotá D.C.">
+    </a>
+</div>
+<div>
+    <nav class="Blog-Noticias">
+        <ul>
+            <li><a href="https://mimillos.com.co/" target="_blank">Blog</a></li>
+            <li><a href="https://tiendamillonarios.com.co/blogs/news" target="_blank">Noticias</a></li>
+        </ul>
+    </nav>
+</div>
+</section>
+</header>`
+
+const Footer = `<section class="footer1">
+<div>
+    <h2>Sobre Nosotros</h2>
+</div>
+<div>
+    <h2>Nosotros Somos Bogotá D.C.</h2>
+</div>
+<div>
+    <p>
+        <b>Direccion: </b> Calle 52 # 12c - 46 - Zona 06 - Tunjuelito - Bogotá D.C.
+        <br>
+        <b>Tel/cel: </b> 601 205 2066 / 322 538 0967
+        <br>
+        <b>Autor: </b>Daniel Sepulveda
+        <br>
+        <b>Brutalmente Embajador</b>
+    </p>
+</div>
+<div>
+    <p>
+        La barra brava del equipo Los Millonarios fue fundada entre 1992 y 1993. Fue creada por
+        jóvenes, quienes se tomaron la parte lateral norte del estadio Nemesio Camacho el Campin de
+        Bogotá, haciendo de esta localidad su casa permanente partido tras partido.
+    </p>
+</div>
+</section>
+<section class="footer2">
+<div id="footer2">
+    <h2>Nuestras Redes</h2>
+</div>
+<div>
+    <a href="https://es-la.facebook.com/millosfcoficial/">
+        <img width="10%" src="Img/fb.png" alt="Facebook">
+    </a>
+</div>
+<div>
+    <a href="https://www.instagram.com/millosfcoficial/?hl=es">
+        <img width="20%" src="Img/instagram.png" alt="Instagram">
+    </a>
+</div>
+<div>
+    <a href="https://twitter.com/MillosFCoficial">
+        <img width="10%" src="Img/X.jpg" alt="X">
+    </a>
+</div>
+</section>`
